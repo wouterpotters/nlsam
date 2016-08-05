b88e37745cf2c7dca2c061595555b42867ecc6bb
Samuel St-Jean <samuel.st-jean@usherbrooke.ca>
2016-08-05 11:40:11 +0200

    stuff

--- nlsam/denoiser.py
+++ nlsam/denoiser.py
@@ -22,7 +22,7 @@ from nlsam.enet import lasso_path, select_best_path
 
 from sklearn.feature_extraction.image import extract_patches
 from scipy.optimize import nnls
-# from skimage.util.shape import view_as_windows
+from skimage.util.shape import view_as_windows, view_as_blocks
 # from sklearn.linear_model import Lasso
 
 warnings.simplefilter("ignore", category=FutureWarning)
@@ -67,14 +67,24 @@ def processer(arglist):
     data, mask, variance, block_size, overlap, param_alpha, param_D, dtype, n_iter = arglist
     return _processer(data, mask, variance, block_size, overlap, param_alpha, param_D, dtype=dtype, n_iter=n_iter)
 
-# def processer(arglist):
-def _processer(data, mask, variance, block_size, overlap, param_alpha, param_D, dtype=np.float64, n_iter=10, gamma=3., tau=1.):
 
+def _processer(data, mask, variance, block_size, overlap, param_alpha, param_D, dtype=np.float64, n_iter=10, gamma=3., tau=1.):
+    # return data
     # mask=np.ones_like(mask)
     # data = data.astype(np.float64)
     orig_shape = data.shape
     extraction_step = np.array([1, 1, 1, block_size[-1]])
-    mask_array = im2col_nd(mask, block_size[:3], overlap[:3])
+
+    no_overlap = False
+
+    if no_overlap:
+        overlap = (0,0,0,0)
+        extraction_step = list(block_size)[:-1] + [1]
+
+    # print(mask.shape, block_size[:-1],  extraction_step[:-1])
+    mask_array = extract_patches(mask, block_size[:-1], extraction_step[:-1]).reshape((-1, np.prod(block_size[:-1]))).T
+    # mask_array = im2col_nd(mask, block_size[:3], overlap[:3])
+
     train_idx = np.sum(mask_array, axis=0) > mask_array.shape[0] / 2
 
     # mask_array = extract_patches(mask, patch_shape=block_size[:3], extraction_step=extraction_step[:3])
@@ -89,19 +99,12 @@ def _processer(data, mask, variance, block_size, overlap, param_alpha, param_D, 
     # X = im2col_nd(data, block_size, overlap)
     # a1 = time()
     D = param_alpha['D']
-    # print(data.shape, extraction_step, block_size)
+
     X = extract_patches(data, patch_shape=block_size, extraction_step=extraction_step)
     X_out = np.zeros((D.shape[0], train_idx.shape[0]), dtype=np.float32)
-    # print(X.shape, X_out.shape)
-    # 1/0
-    # print(X.shape)
-    # X = X.reshape([-1] + list(block_size))
-    # X = X.reshape(X.shape[0], -1).T
-    # a2 = time()
-    # X2 = im2col_nd(data, block_size, overlap)
-    # print(np.sum(np.abs(X - X2)), time() - a2, a2 - a1)
-    # 1/0
-    var_mat = np.median(im2col_nd(variance[..., 0:orig_shape[-1]], block_size, overlap), axis=0).astype(np.float32)
+
+    # var_mat = np.median(im2col_nd(variance[..., 0:orig_shape[-1]], block_size, overlap), axis=0).astype(np.float32)
+    var_mat = None
     # X_full_shape = X.shape
     # X = X[:, train_idx].astype(np.float32)
     # var_mat = np.ones_like(var_mat)
@@ -113,64 +116,57 @@ def _processer(data, mask, variance, block_size, overlap, param_alpha, param_D, 
         D_norm = (D - M) / S
         return D_norm, M, S
 
-    nlam = 30  #len(lambdas)
     # lambdas = np.logspace(np.log10(lambda_max * eps), np.log10(lambda_max), num=nlam)[::-1]
-    # print(lambdas)
-    # 1/0
-    # lambdas=[0.0015]
+    nlam = 50  #len(lambdas)
     Xhat = np.zeros((D.shape[0], nlam), dtype=np.float64)
     alphas = np.zeros((D.shape[1], nlam), dtype=np.float64)
     best = np.zeros(X_out.shape[1])
-    # Xhat = np.zeros((X.shape[0], nlam), dtype=np.float32)
-    # alphas = np.zeros((D.shape[1], nlam), dtype=np.float32)
-    # Xhat = np.zeros_like(Xs, dtype=np.float32)
-    # print(Xhat.shape, alphas.shape)
 
-    for i, idx in enumerate(ndindex(X.shape[:X.ndim//2])):
-        # print(i, X.shape[1], idx)
+    # md = D.mean(axis=0, keepdims=True)
+    #
+    # print(D.shape, md.shape)
+    # 1/0
+    for i, idx in enumerate(ndindex(X.shape[:X.ndim // 2])):
+
         if not train_idx[i]:
             continue
-        # print(i, X.shape[1], idx)
-        # alphas[:] = lasso_cd(X[:, i:i+1], D).squeeze()
-        # Xhat[:] = np.dot(D, alphas).squeeze()
-        # X[:, i] = np.dot(D, alpha[:, i]).squeeze()
-        # # X[:, i], m, s = mystandardize(X[:, i])
-        # print(D.shape, X[idx].shape, X[idx].ravel().shape, Xhat.shape, alphas.shape)
-        # xm = np.mean(X[idx])
-        # X[idx] -= xm
-        # xx,m,s = mystandardize(X[idx].ravel())
-        # m = X[idx].mean()
-        # X[idx] -= m
-        Xhat[:], alphas[:] = lasso_path(D, X[idx], nlam=nlam, intr=True, pos=True, isd=True, ols=False)#, lambdas=lambdas, maxit=10000, )
-        # Xhat[:] += m
-        # X[idx] += m
-        # Xhat[:] * s + m
-        # Xhat += xm
-        # # var_mat[i]=1
-        # print(D.shape, X[:, i].shape, alphas.shape, Xhat.shape, var_mat[i].shape)
-        X_out[:, i], alpha[:, i], best[i] = select_best_path(D, X[idx], alphas, Xhat, var_mat[i], criterion='ric')
 
-    # try:
-    #     infile = np.load(open('arr.npy', 'r'))
-    # except IOError:
-    #     infile = np.zeros(1)
-    # with open('arr.npy', 'w') as file:
-    #     np.save(file, np.append(infile, best))
+        # mx = np.mean(X[idx])
+        # X[idx] -= mx
+        # D -= mx
 
-    # print(np.sum(alpha<0), np.sum(X_out<0), np.sum(D<0), np.sum(X<0), np.sum(data < 0))
-        # print(np.sum(np.isnan(Xhat)), np.sum(np.isnan(alphas)))
-        # # X[:, i], alpha[:, i] = Xhat[:, -1], alphas[:, -1]
-        # # X[:, i] = (X[:, i] * s) + m
+        Xhat[:], alphas[:] = lasso_path(D, X[idx], nlam=nlam, intr=True, pos=True, isd=False, ols=False)#, lambdas=lambdas, maxit=10000, )
+        X_out[:, i], alpha[:, i], best[i] = select_best_path(D, X[idx], alphas, Xhat, var_mat, criterion='bic')
+        # X_out[:, i] += mx
+        # D += mx
+    # D += md
+
 
     weigths = np.ones(X_out.shape[1], dtype=dtype, order='F')
     # weigths[train_idx] = 1. / ((alpha != 0).sum(axis=0) + 1.)
-    # print(type(X), X.shape)
-    # X2 = np.zeros(X_full_shape, dtype=dtype, order='F')
-    # # print(X2.shape, X.shape, D.shape, alpha.shape)
-    # X2[:, train_idx] = X
+
+    if no_overlap:
+        out = np.zeros_like(data)
+        shaper = np.array(out.shape[:-1]) - np.array(block_size[:-1]) + 1
+        shaper = shaper[:-1]
+
+        # shaper = (range(0, (out.shape[0]//block_size[0] + 1) * block_size[0], block_size[0]),
+        #           range(0, (out.shape[1]//block_size[1] + 1) * block_size[1], block_size[1]),
+        #           range(0, (out.shape[2]//block_size[2] + 1) * block_size[2], block_size[2]))
+        shaper = (range(0, out.shape[0], block_size[0]),
+                  range(0, out.shape[1], block_size[1]),
+                  range(0, out.shape[2], block_size[2]))
+        idx = 0
+        for i in shaper[0]:
+            for j in shaper[1]:
+                for k in shaper[2]:
+
+                    out[i:i + block_size[0], j:j + block_size[1]] += X_out[:, idx].reshape(block_size)
+                    idx += 1
+
+        return out
 
     return col2im_nd(X_out, block_size, orig_shape, overlap, weigths)
-    # return col2im_nd(X2, block_size, orig_shape, overlap, weigths)
 
 
 def denoise(data, block_size, overlap, param_alpha, param_D, variance, n_iter=10,
@@ -189,12 +185,26 @@ def denoise(data, block_size, overlap, param_alpha, param_D, variance, n_iter=10
     # c=time()
     # X2 = X2.reshape(X2.shape[0], -1).T
     # b=time()
-
+    # data2 = np.zeros((129,129,78,6))
+    # data2[:128,:128] = data[:]
+    # data=data2
     # no overlapping blocks for training
-    no_over = (0, 0, 0, 0)
-    X = im2col_nd(data, block_size, no_over)
+    # a=time()
 
-    # print(data.shape, block_size)
+    # no_over = (0, 0, 0, 0)
+    # X = im2col_nd(data, block_size, no_over)
+    # print(data.shape, block_size, X.shape, time()-a)
+
+    # a=time()
+    patch_shape = block_size
+    extraction_step = list(block_size)[:-1] + [1]
+    # extraction_step=1
+    X = extract_patches(data, patch_shape, extraction_step).reshape((-1, np.prod(patch_shape))).T
+    # X = X.reshape(X.shape, )
+    # print(data.shape, block_size, X2.shape, time()-a, X2.flags)
+    # print(np.sum(np.abs(X-X2)))
+
+    # 1/0
     # t=time()
     # print(t-b, t-a, b-c,c-a1,a1-a)#, np.sum(np.abs(X-X2)))
     # # 1/0
@@ -207,9 +217,10 @@ def denoise(data, block_size, overlap, param_alpha, param_D, variance, n_iter=10
     param_D['verbose'] = False
     param_D['posAlpha'] = True
     param_D['posD'] = True
+    # param_D['mode'] = 5
     param_D['mode'] = 2
     param_D['lambda1'] = 1.2 / np.sqrt(np.prod(block_size))
-    param_D['K'] = int(2*np.prod(block_size))
+    param_D['K'] = int(2 * np.prod(block_size))
     param_D['iter'] = 150
     param_D['batchsize'] = 500
 
@@ -218,7 +229,8 @@ def denoise(data, block_size, overlap, param_alpha, param_D, variance, n_iter=10
 
     # a=time()
     # mask_col = im2col_nd(mask, block_size[:3], no_over[:3])
-    mask_col = im2col_nd(np.broadcast_to(mask[..., None], data.shape), block_size, no_over)
+    # mask_col = im2col_nd(np.broadcast_to(mask[..., None], data.shape), block_size, no_over)
+    mask_col = extract_patches(mask, patch_shape[:-1], extraction_step[:-1]).reshape((-1, np.prod(patch_shape[:-1]))).T
     train_idx = np.sum(mask_col, axis=0) > mask_col.shape[0]/2
     # print(train_idx.shape)
     # mask_array = extract_patches(mask, patch_shape=block_size, extraction_step=block_size)
@@ -227,7 +239,7 @@ def denoise(data, block_size, overlap, param_alpha, param_D, variance, n_iter=10
     # train_idx = np.sum(mask_array.reshape([-1] + list(block_size[:2])), axis=(1, 2)) > mask_array.shape[0] / 2
     # print(train_idx.shape)
     # 1/0
-
+    # print(train_idx.shape)
     train_data = X[:, train_idx]
     # print(train_idx.shape, train_data.shape)
     # print(np.sum(train_idx), np.sum( np.any(train_data != 0, axis=0)))
@@ -242,12 +254,14 @@ def denoise(data, block_size, overlap, param_alpha, param_D, variance, n_iter=10
     # print(train_idx.shape, X.shape, train_data.shape)
     # print(time()-a)
     # 1/0
+    # a=time()
     param_alpha['D'] = spams.trainDL(train_data, **param_D)
     param_alpha['D'] /= np.sqrt(np.sum(param_alpha['D']**2, axis=0, keepdims=True, dtype=np.float64))
     param_D['D'] = param_alpha['D']
 
-    del train_data
-
+    del train_data, X
+    # print(time() - a)
+    # 1/0
     n_cores = param_alpha['numThreads']
     param_alpha['numThreads'] = 1
     param_D['numThreads'] = 1
@@ -256,9 +270,16 @@ def denoise(data, block_size, overlap, param_alpha, param_D, variance, n_iter=10
     pool = Pool(processes=n_cores)
     # a=time()
 
+    overlap = True
+
+    if overlap:
+        ranger = range(data.shape[2] - block_size[2] + 1)
+    else:
+        ranger = range(0, data.shape[2], block_size[2])
+
     arglist = [(data[:, :, k:k+block_size[2]], mask[:, :, k:k+block_size[2]], variance[:, :, k:k+block_size[2]], block_size_subset, overlap_subset, param_alpha_subset, param_D_subset, dtype_subset, n_iter_subset)
                for k, block_size_subset, overlap_subset, param_alpha_subset, param_D_subset, dtype_subset, n_iter_subset
-               in zip(range(data.shape[2] - block_size[2] + 1),
+               in zip(ranger,
                       repeat(block_size),
                       repeat(overlap),
                       repeat(param_alpha),
@@ -281,9 +302,10 @@ def denoise(data, block_size, overlap, param_alpha, param_D, variance, n_iter=10
     data_subset = np.zeros_like(data)
     divider = np.zeros_like(data, dtype=np.int16)
     ones = np.ones_like(data_denoised[0], dtype=np.int16)
-
-    for k in range(len(data_denoised)):
-        data_subset[:, :, k:k+block_size[2]] += data_denoised[k]
+    # print(len(data_denoised), data_subset.shape, divider.shape, ones.shape)
+    # for k in range(len(data_denoised)):
+    for i, k in enumerate(ranger):
+        data_subset[:, :, k:k+block_size[2]] += data_denoised[i]
         divider[:, :, k:k+block_size[2]] += ones
 
     data_subset /= divider
--- nlsam/enet.py
+++ nlsam/enet.py
@@ -5,28 +5,6 @@ from scipy.sparse import csr_matrix
 from scipy.optimize import nnls
 from scipy.linalg import lstsq
 
-# class ElasticNet(object):
-#     """ElasticNet based on GLMNET"""
-#     def __init__(self, alpha, rho=0.2):
-#         super(ElasticNet, self).__init__()
-#         self.alpha = alpha
-#         self.rho = rho
-#         self.coef_ = None
-#         self.rsquared_ = None
-
-#     def fit(self, X, y):
-#         n_lambdas, intercept_, coef_, _, _, rsquared_, lambdas, _, jerr \
-#         = elastic_net(X, y, self.rho, lambdas=[self.alpha])
-#         # elastic_net will fire exception instead
-#         # assert jerr == 0
-#         self.coef_ = coef_
-#         self.intercept_ = intercept_
-#         self.rsquared_ = rsquared_
-#         return self
-
-#     def predict(self, X):
-#         return np.dot(X, self.coef_) + self.intercept_
-
 
 def elastic_net_path(X, y, rho, ols=False, **kwargs):
     """return full path for ElasticNet"""
@@ -91,13 +69,15 @@ def select_best_path(X, y, beta, mu, variance=None, criterion='aic'):
     #     mu[:, i] = np.dot(X, beta[i]) + intercepts[i]
 
     # print(X.shape, y.shape, beta.shape, mu.shape, variance.shape)
-    mse = np.mean((y[..., None] - mu)**2, axis=0, dtype=np.float32)
-    df_mu = np.sum(beta != 0, axis=0, dtype=np.int32)
+    mse = np.mean((y[..., None] - mu)**2, axis=0) #, dtype=np.float32)
+    df_mu = np.sum(beta != 0, axis=0) #, dtype=np.int32)
+    # print(mse.shape, df_mu.shape, mu.shape, X.shape, y.shape)
+    # 1/0
     # print(mu.shape, df_mu.shape, beta.shape, variance[...,None].shape, sse.shape, y.shape)
     # variance=None
 
     # Use SSE/n estimate for sample variance - we assume normally distributed
-    # residuals though for the log likelihood function...
+    # residuals though for the log-likelihood function...
     if variance is None:
         criterion = n * np.log(mse) + df_mu * w
         # s2 = sse / n
@@ -108,175 +88,14 @@ def select_best_path(X, y, beta, mu, variance=None, criterion='aic'):
 
     # We don't want empty models
     criterion[df_mu == 0] = 1e300
-
     best_idx = np.argmin(criterion, axis=0)
-    # print(best_idx)
 
-    # print(mu.shape, best_idx.shape, beta.shape)
-    # print(best_idx)
-    # print(criterion.shape, df_mu.shape, mu.shape)
-    # # print(criterion)
-    # print(mu.shape, beta.shape)
-    # print(mu[:, best_idx].shape, beta[:, best_idx].shape)
-    # for i in range(100):
-    #     print(mu[i])
-    # print(best_idx, criterion[best_idx-1], criterion[best_idx])
-    # print(criterion)
     return mu[:, best_idx], beta[:, best_idx], best_idx
-    # best_betas = np.empty(beta.shape[:-1], dtype=np.float32)
-    # best_mu = np.empty(y.shape, dtype=np.float32)
-    # print(criterion.shape, best_idx.shape, best_betas.shape, beta.shape, best_mu.shape, mu.shape)
-    # for i in range(criterion.shape[0]):
-    #     # print(best_idx[i], )
-    #     best_betas[:, i] = beta[:, i, best_idx[i]]
-    #     best_mu[:, i] = mu[:, i, best_idx[i]]
-    # # print(criterion[10])
-    # return best_mu, best_betas
-
-# def Lasso(alpha):
-#     """Lasso based on GLMNET"""
-#     return ElasticNet(alpha, rho=1.0)
 
 
 def lasso_path(X, y, ols=False, **kwargs):
     """return full path for Lasso"""
-
-    # from glmnet import ElasticNet
-    # enet = ElasticNet(alpha=1.)
-    # enet.fit(X, y, normalize=False, include_intercept=False, **kwargs)
-
-    # nlam = enet.n_lambdas
-    # yhat = np.zeros((y.shape[0], nlam))
-    # alpha = np.zeros((X.shape[1], nlam))
-
-    # for i in range(nlam):
-    #     yhat[:, i], alpha[:, i] = enet.predict(X).squeeze(), enet.get_coefficients_from_lambda_idx(i)
-    # return yhat, alpha
-    # # return enet.get_coefficients_from_lambda_idx(0)
     return elastic_net_path(X, y, rho=1.0, ols=ols, **kwargs)
-
-
-# def __elastic_net(X, y, rho, **kwargs):
-
-#     # out = [None] * y.shape[1]
-#     # for i in range(y.shape[1]):
-#     #     out[i] =
-#     # print(len(out))
-
-#     n_lambdas, intercepts, coefs_, indices, nin, _, lambdas, _, jerr = _elastic_net(X, y, rho, **kwargs)
-
-#     return n_lambdas, intercepts[None, :], coefs_[None, :], indices[None, :], nin[None, :], _, lambdas[None, :], _, jerr
-
-
-# def _elastic_net(predictors, target, balance, memlimit=None,
-#                  largest=None, **kwargs):
-#     """
-#     Raw-output wrapper for elastic net linear regression.
-#     """
-#     _DEFAULT_THRESH = 1.0e-4
-#     _DEFAULT_FLMIN = 0.001
-#     _DEFAULT_NLAM = 100
-#     # Mandatory parameters
-#     predictors = np.asanyarray(predictors)
-#     target = np.asanyarray(target)
-
-#     # Decide on largest allowable models for memory/convergence.
-#     memlimit = predictors.shape[1] if memlimit is None else memlimit
-
-#     # If largest isn't specified use memlimit.
-#     largest = memlimit if largest is None else largest
-
-#     if memlimit < largest:
-#         raise ValueError('Need largest <= memlimit')
-
-#     # Flags determining overwrite behavior
-#     overwrite_pred_ok = False
-#     overwrite_targ_ok = False
-
-#     thr = _DEFAULT_THRESH   # Minimum change in largest coefficient
-#     weights = None          # Relative weighting per observation case
-#     vp = None               # Relative penalties per predictor (0 = no penalty)
-#     isd = True              # Standardize input variables before proceeding?
-#     jd = np.zeros(1)        # Predictors to exclude altogether from fitting
-#     ulam = None             # User-specified lambda values
-#     flmin = _DEFAULT_FLMIN  # Fraction of largest lambda at which to stop
-#     nlam = _DEFAULT_NLAM    # The (maximum) number of lambdas to try.
-
-#     for keyword in kwargs:
-#         if keyword == 'overwrite_pred_ok':
-#             overwrite_pred_ok = kwargs[keyword]
-#         elif keyword == 'overwrite_targ_ok':
-#             overwrite_targ_ok = kwargs[keyword]
-#         elif keyword == 'threshold':
-#             thr = kwargs[keyword]
-#         elif keyword == 'weights':
-#             weights = np.asarray(kwargs[keyword]).copy()
-#         elif keyword == 'penalties':
-#             vp = kwargs[keyword].copy()
-#         elif keyword == 'standardize':
-#             isd = bool(kwargs[keyword])
-#         elif keyword == 'exclude':
-#             # Add one since Fortran indices start at 1
-#             exclude = (np.asarray(kwargs[keyword]) + 1).tolist()
-#             jd = np.array([len(exclude)] + exclude)
-#         elif keyword == 'lambdas':
-#             if 'flmin' in kwargs:
-#                 raise ValueError("Can't specify both lambdas & flmin keywords")
-#             ulam = np.asarray(kwargs[keyword])
-#             flmin = 2. # Pass flmin > 1.0 indicating to use the user-supplied.
-#             nlam = len(ulam)
-#         elif keyword == 'flmin':
-#             flmin = kwargs[keyword]
-#             ulam = None
-#         elif keyword == 'nlam':
-#             if 'lambdas' in kwargs:
-#                 raise ValueError("Can't specify both lambdas & nlam keywords")
-#             nlam = kwargs[keyword]
-#         else:
-#             raise ValueError("Unknown keyword argument '%s'" % keyword)
-
-#     box_constraints = np.zeros((2, predictors.shape[1]), order='F')
-#     box_constraints[1] = 1e300
-#     # box_constraints[0] = -1e300
-
-#     # If predictors is a Fortran contiguous array, it will be overwritten.
-#     # Decide whether we want this. If it's not Fortran contiguous it will
-#     # be copied into that form anyway so there's no chance of overwriting.
-#     if np.isfortran(predictors):
-#         if not overwrite_pred_ok:
-#             # Might as well make it F-ordered to avoid ANOTHER copy.
-#             predictors = predictors.copy(order='F')
-
-#     # target being a 1-dimensional array will usually be overwritten
-#     # with the standardized version unless we take steps to copy it.
-#     if not overwrite_targ_ok:
-#         target = target.copy()
-
-#     # Uniform weighting if no weights are specified.
-#     if weights is None:
-#         weights = np.ones(predictors.shape[0])
-
-#     # Uniform penalties if none were specified.
-#     if vp is None:
-#         vp = np.ones(predictors.shape[1])
-
-#     # Call the Fortran wrapper.
-#     lmu, a0, ca, ia, nin, rsq, alm, nlp, jerr =  \
-#             _glmnet.elnet(balance, predictors, target, weights, jd, vp, box_constraints,
-#                           memlimit, flmin, ulam, thr, nlam=nlam)
-
-#     # Check for errors, documented in glmnet.f.
-#     if jerr != 0:
-#         if jerr == 10000:
-#             raise ValueError('cannot have max(vp) < 0.0')
-#         elif jerr == 7777:
-#             raise ValueError('all used predictors have 0 variance')
-#         elif jerr < 7777:
-#             raise MemoryError('elnet() returned error code %d' % jerr)
-#         else:
-#             raise Exception('unknown error: %d' % jerr)
-
-#     return lmu, a0, ca, ia, nin, rsq, alm, nlp, jerr
 
 
 def elastic_net(X, y, rho, pos=True, thr=1.0e-4, weights=None, vp=None,
@@ -365,6 +184,7 @@ def elastic_net(X, y, rho, pos=True, thr=1.0e-4, weights=None, vp=None,
         weights = np.ones(X.shape[0], order='F')
     else:
         weights = np.asarray(weights).copy()
+
     # Uniform penalties if none were specified.
     if vp is None:
         vp = np.ones(X.shape[1], order='F')
@@ -372,109 +192,13 @@ def elastic_net(X, y, rho, pos=True, thr=1.0e-4, weights=None, vp=None,
         vp = vp.copy()
 
     # Call the Fortran wrapper.
-
     nx = X.shape[1]
-    # ny = y.shape[1]
-
-    # lmu = np.zeros(( dtype=np.int32)
-    # a0 = np.zeros(nlam, dtype=np.float32)
-    # ca = np.zeros((nx, nlam), dtype=np.float32)
-    # # ca_ = np.zeros((nx, nlam), dtype=np.float32)
-    # ia = np.zeros(nx, dtype=np.int32)
-    # nin = np.zeros(nlam, dtype=np.int32)
-    # rsq = np.zeros(nlam, dtype=np.float32)
-    # alm = np.zeros(nlam, dtype=np.float32)
-    # print(a0.shape, ca.shape, X.shape, y.shape)
-    # for idx in range(y.shape[1]):
 
     lmu, a0, ca, ia, nin, rsq, alm, nlp, jerr = \
         elnet(rho, X, y, weights, jd, vp, box_constraints, nx, flmin, ulam, thr,
               nlam=nlam, isd=isd, maxit=maxit, intr=intr)
 
     return lmu, a0, ca, ia, nin, rsq, alm, nlp, jerr
-
-
-# class GlmnetLinearModel(object):
-#     """Class representing a linear model trained by Glmnet."""
-#     def __init__(self, a0, ca, ia, nin, rsq, alm, npred):
-#         self._intercept = a0
-#         self._coefficients = ca[:nin]
-#         self._indices = ia[:nin] - 1
-#         self._rsquared = rsq
-#         self._lambda = alm
-#         self._npred = npred
-
-
-#     def predict(self, X):
-#         X = np.atleast_2d(np.asarray(X))
-#         return self._intercept + \
-#                 np.dot(X[:, self._indices], self._coefficients)
-
-#     @property
-#     def coefficients(self):
-#         coeffs = np.zeros(self._npred)
-#         coeffs[self._indices] = self._coefficients
-#         return coeffs
-
-
-# class GlmnetLinearResults(object):
-#     def __init__(self, lmu, a0, ca, ia, nin, rsq, alm, nlp, npred, parm):
-#         self._lmu = lmu
-#         self._a0 = a0
-#         self._ca = ca
-#         self._ia = ia
-#         self._nin = nin
-#         self._rsq = rsq
-#         self._alm = alm
-#         self._nlp = nlp
-#         self._npred = npred
-#         self._model_objects = {}
-#         self._parm = parm
-
-#     def __len__(self):
-#         return self._lmu
-
-#     def __getitem__(self, item):
-#         item = (item + self._lmu) if item < 0 else item
-#         if item >= self._lmu or item < 0:
-#             raise IndexError("model index out of bounds")
-
-#         if item not in self._model_objects:
-#             model =  GlmnetLinearModel(
-#                         self._a0[item],
-#                         self._ca[:,item],
-#                         self._ia,
-#                         self._nin[item],
-#                         self._rsq[item],
-#                         self._alm[item],
-#                         self._npred
-#                     )
-#             self._model_objects[item] = model
-
-#         else:
-#             model = self._model_objects[item]
-
-#         return model
-
-#     @property
-#     def nummodels(self):
-#         return self._lmu
-
-#     @property
-#     def coefficients(self):
-#         return self._ca[:np.max(self._nin), :self._lmu]
-
-#     @property
-#     def indices(self):
-#         return self._ia
-
-#     @property
-#     def lambdas(self):
-#         return self._alm[:self._lmu]
-
-#     @property
-#     def rho(self):
-#         return self._parm
 
 
 # This part stolen from
